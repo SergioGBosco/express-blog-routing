a78d77e64107a866b7c8a0a8fb29f1dba5448292
SergioGBosco <sergiogibosco@gmail.com>
2025-08-04 15:21:11 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,26 @@
+const express = require("express");
+
+//inizializzo laa variabine app di express
+const app = express();
+
+//definisco la porta
+
+const port = 3000;
+
+//aggiungo gli asset statici
+
+app.use(express.static(`public`))
+
+//*definisco la rotta base del server
+
+app.get("/", (req, res) => {
+  res.send("Creata rotta base del sel server")
+});
+
+app.listen(port, () => {
+  console.log(`server del blog in attesa nella porta ${port}`)
+
+})
+
+
+
